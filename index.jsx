b9c896f78555c717b2ac7343b3133b79d93bdd59
sam schonstal <sam@schonstal.com>
2016-06-30 11:37:30 -0700

    nested react elements

--- index.jsx
+++ index.jsx
@@ -1,4 +1,7 @@
+var child =  React.createElement('div', null, 'Hello Sam');
+var parent =  React.createElement('div', {className: 'green-box'}, child);
+
 ReactDOM.render(
-  React.createElement('div', null, "Hello Sam"),
+  parent,
   document.getElementById('content')
 );
