8d850ac92ff7b2e999cd27454310c441d524d6da
sam schonstal <sam@schonstal.com>
2016-07-01 09:40:28 -0700

    javascript loop

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var child =  React.createElement('div', null, 'Hello Sam');
-var parent =  React.createElement('div', {className: 'green-box'}, child);
+var children =  [];
+
+for (var i = 0; i < 10; i++) {
+  children.push(React.createElement('div', null, 'Hello React JS ' + i))
+}
+
+var parent =  React.createElement('div', {className: 'green-box'}, children);
 
 ReactDOM.render(
   parent,
