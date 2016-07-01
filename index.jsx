1b4888aa836958498bdcc84a8799f88bc81158c0
sam schonstal <sam@schonstal.com>
2016-07-01 15:42:50 -0700

    prop example

--- index.jsx
+++ index.jsx
@@ -1,23 +1,15 @@
 
 class HelloBox extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      counter: 0
-    };
-    setInterval(() => {this.setState({counter: this.state.counter + 1 })}, 500)
-  }
   render() {
     return (
       <div className='green-box'>
-          <textarea rows="1" cols="20"/>
-          <div style={{ fontSize: '200%' }}>counter {this.state.counter}</div>
-        </div>
+        <div style={{ fontSize: '200%' }}>Hello {this.props.name}</div>
+      </div>
     );
   }
 }
 
 ReactDOM.render(
-  <HelloBox/>,
+  <HelloBox name='from prop' />,
   document.getElementById('content')
 );
