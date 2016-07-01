04af4efce993a7f7db173fadc60e577792c4ba21
sam schonstal <sam@schonstal.com>
2016-07-01 10:30:53 -0700

    nested component

--- index.jsx
+++ index.jsx
@@ -1,16 +1,24 @@
 
-let name = 'ES6 Class';
+let name = 'Nested Components';
 class HelloBox extends React.Component {
   render() {
     return (
     <div className='green-box'>
-      <div style={{ fontSize: '200%' }}>Hello {name}</div>
+      <BoxContents/>
     </div>
     );
   }
 }
 
+class BoxContents extends React.Component {
+  render() {
+    return (
+        <div style={{ fontSize: '200%' }}>Hello {name}</div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <HelloBox />,
+  <HelloBox/>,
   document.getElementById('content')
 );
