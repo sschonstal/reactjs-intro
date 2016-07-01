bbe34c3e701d809d0fe4f18cf06ae61935966e5e
sam schonstal <sam@schonstal.com>
2016-07-01 10:45:35 -0700

    state demo

--- index.jsx
+++ index.jsx
@@ -11,9 +11,19 @@ class HelloBox extends React.Component {
 }
 
 class BoxContents extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+    setInterval(() => {this.setState({counter: this.state.counter + 1 })}, 500)
+  }
   render() {
     return (
-        <div style={{ fontSize: '200%' }}>Hello {name}</div>
+        <div>
+          <textarea rows="1" cols="20"/>
+          <div style={{ fontSize: '200%' }}>counter {this.state.counter}</div>
+        </div>
     );
   }
 }
