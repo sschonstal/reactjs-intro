e13500dee5fe3739e74f830520b9e29c49bfe479
sam schonstal <sam@schonstal.com>
2016-07-05 09:53:04 -0700

    composing components

--- index.jsx
+++ index.jsx
@@ -1,15 +1,27 @@
-
-class HelloBox extends React.Component {
-  render() {
+class HelloContent extends React.Component {
+  render () {
     return (
-      <div className='green-box'>
+      <div>
+        <div className='logo-container'>
+          <img src={this.props.logo} className='logo'/>
+        </div>
         <div style={{ fontSize: '200%' }}>Hello {this.props.name}</div>
       </div>
     );
   }
 }
 
+class HelloBox extends React.Component {
+  render () {
+    return (
+      <div className='green-box'>
+        <HelloContent name='from prop parent' logo="./images/vett.jpg" />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <HelloBox name='from prop' />,
+  <HelloBox name='from prop parent' logo="./images/vett.jpg"/>,
   document.getElementById('content')
 );
