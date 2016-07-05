41e02db0dc409e737aa97f6247f8aebb457ccb51
sam schonstal <sam@schonstal.com>
2016-07-05 10:10:22 -0700

    add image html

--- index.jsx
+++ index.jsx
@@ -3,6 +3,9 @@ class HelloBox extends React.Component {
   render() {
     return (
       <div className='green-box'>
+        <div className='logo-container'>
+          <img src={this.props.logo} className='logo'/>
+        </div>
         <div style={{ fontSize: '200%' }}>Hello {this.props.name}</div>
       </div>
     );
@@ -10,6 +13,6 @@ class HelloBox extends React.Component {
 }
 
 ReactDOM.render(
-  <HelloBox name='from prop' />,
+  <HelloBox name='from prop' logo="./images/vett.jpg"/>,
   document.getElementById('content')
 );
