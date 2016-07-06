9a8f7de598c24904b7b242e26a56297b659e77f6
sam schonstal <sam@schonstal.com>
2016-07-06 08:49:49 -0700

    style to row format

--- index.jsx
+++ index.jsx
@@ -1,44 +1,47 @@
-
-function HelloContent(props)  {
-    return (
-      <div>
-        <div className='logo-container'>
-          <img src={props.car} className='logo'/>
-        </div>
-        <div style={{ fontSize: '200%' }}>Hello {props.name}</div>
+function HelloContent (props) {
+  return (
+    <div style={{ float: "left" }}>
+      <div className='logo-container'>
+        <img src={props.car.img} className='logo'/>
       </div>
-    )
+      <div className="content-text">{props.car.name}</div>
+    </div>
+  )
 }
 
 class HelloBox extends React.Component {
-  constructor() {
+  constructor () {
     super()
     this.state = {
       index: 0
     }
-   this.next = this.next.bind(this)
+    this.next = this.next.bind(this)
   }
 
   componentWillMount () {
     this.setState({
-      cars: [ "./images/vett.jpg",
-        "./images/camero.jpeg",
-        "./images/challanger.jpeg",
-        "./images/gt.jpg",
-        "./images/viper.jpg",
-        "./images/mustang.jpg" ]
+      cars: [ {img: "./images/vett.jpg", name: "Chevrolet Corvett" },
+        {img: "./images/camero.jpeg", name: "Chevrolet Camero" },
+        {img: "./images/challanger.jpeg", name: "Dodge Challanger" },
+        {img: "./images/gt.jpg", name: "Ford GT" },
+        {img: "./images/viper.jpg", name: "Dodge viper" },
+        {img: "./images/mustang.jpg", name: "Ford Mustang" } ]
     })
   }
-  next() {
-      this.setState({
+
+  next () {
+    this.setState({
       index: this.state.index === this.state.cars.length - 1 ? 0 : this.state.index + 1
     })
   }
+
   render () {
     return (
       <div className='green-box'>
-        <HelloContent {...this.props} car={this.state.cars[this.state.index]} />
-        <button  type="button" onClick={this.next}>Next </button>
+        <HelloContent {...this.props} car={this.state.cars[this.state.index]}/>
+        <div className="btn-container">
+          <button className="btn" type="button" onClick={this.next}>Next</button>
+        </div>
       </div>
     );
   }
